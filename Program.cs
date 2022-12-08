4b2fa093b80b7c01ea1e16cea7db02a363e2fe9a
Martin Weber <14039505+martin-weber@users.noreply.github.com>
2022-12-08 15:33:49 +0100

    Fizz

--- Program.cs
+++ Program.cs
@@ -16,6 +16,25 @@
     {
         public List<string> Run(int number)
         {
+            if((0 > number) || (number > 100))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    var temp = "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+
+                }
+            }
+
             return new List<string>();
         }
     }
