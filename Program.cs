93cebf7a9ec98ba6c78f8d83bc6539798c9fd1c2
Isabel Cabezas <isacabe@microsoft.com>
2022-12-08 15:12:59 +0100

    class creation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace mobprogramming
+{
+
+    public void main()
+    { 
+        
+    
+    }
+
+    public class Fizzbuzz
+    {
+        public Fizzbuzz(int number)
+        {
+        }
+    }
+
+
+}
+// See https://aka.ms/new-console-template for more information
+
+
+
